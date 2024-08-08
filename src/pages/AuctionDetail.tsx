faa07be36be24dafac05358d1845ccd4cae42a99
Oh-Donghan <odonghan94@gmail.com>
2024-08-08 16:49:10 +0900

    design: 상세페이지 CSS및 레이아웃 완료

--- src/pages/AuctionDetail.tsx
+++ src/pages/AuctionDetail.tsx
@@ -8,13 +8,26 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  List,
+  ListIcon,
+  ListItem,
 } from '@chakra-ui/react';
+import { useState } from 'react';
+import { FaUserCheck } from 'react-icons/fa';
 import { RxChevronRight } from 'react-icons/rx';
 
+const imageList = [1, 2, 3, 4, 5, 6];
+
 const AuctionDetail = () => {
+  const [imageSet, setImageSet] = useState(1);
+
+  const handleClick = (ele: number) => {
+    setImageSet(ele);
+  };
+
   return (
-    <div className="px-4 max-w-screen-2xl flex flex-col mx-auto text-18px mb-8">
-      <Breadcrumb className="py-4" spacing="8px" separator={<RxChevronRight />}>
+    <div className="px-6 max-w-screen-xl flex flex-col mx-auto text-xm mb-24">
+      <Breadcrumb className="py-4 px-6" spacing="8px" separator={<RxChevronRight />}>
         <BreadcrumbItem>
           <BreadcrumbLink href="#">전체</BreadcrumbLink>
         </BreadcrumbItem>
@@ -25,14 +38,76 @@ const AuctionDetail = () => {
           <BreadcrumbLink>경매 아이템 이름</BreadcrumbLink>
         </BreadcrumbItem>
       </Breadcrumb>
-      <div className="bg-amber-100 flex gap-8">
-        <section className="bg-amber-300 flex flex-col w-1/2">
-          <div>이미지</div>
-          <div>입찰 목록</div>
+      <div className="flex gap-6 max-lg:flex-col items-center">
+        <section className="flex flex-col w-1/2 px-6 gap-6">
+          <div className="flex gap-3">
+            <ul className="flex flex-col gap-2">
+              {imageList.map((ele, idx) => (
+                <li
+                  key={idx}
+                  onClick={() => handleClick(ele)}
+                  className="w-16 h-20 flex items-center justify-center bg-slate-400 cursor-pointer"
+                >
+                  {ele}
+                </li>
+              ))}
+            </ul>
+            <div className="w-full">
+              <div className="bg-slate-600 aspect-4/5 flex items-center justify-center">
+                {imageSet}
+              </div>
+            </div>
+          </div>
+          <div className="w-480px ml-auto">
+            <List spacing={3} className="p-6 border border-black">
+              <ListItem className="flex justify-between">
+                <div>
+                  <ListIcon as={FaUserCheck} color="green.500" />
+                  dsad*** - 95,000원 입찰 하셨습니다.
+                </div>
+                <span className="text-sm">1시간 전</span>
+              </ListItem>
+              <ListItem className="flex justify-between">
+                <div>
+                  <ListIcon as={FaUserCheck} color="green.500" />
+                  dsad*** - 95,000원 입찰 하셨습니다.
+                </div>
+                <span className="text-sm">1시간 전</span>
+              </ListItem>
+              <ListItem className="flex justify-between">
+                <div>
+                  <ListIcon as={FaUserCheck} color="green.500" />
+                  dsad*** - 95,000원 입찰 하셨습니다.
+                </div>
+                <span className="text-sm">1시간 전</span>
+              </ListItem>
+              <ListItem className="flex justify-between">
+                <div>
+                  <ListIcon as={FaUserCheck} color="green.500" />
+                  dsad*** - 95,000원 입찰 하셨습니다.
+                </div>
+                <span className="text-sm">1시간 전</span>
+              </ListItem>
+              <ListItem className="flex justify-between">
+                <div>
+                  <ListIcon as={FaUserCheck} color="green.500" />
+                  dsad*** - 95,000원 입찰 하셨습니다.
+                </div>
+                <span className="text-sm">1시간 전</span>
+              </ListItem>
+              <ListItem className="flex justify-between">
+                <div>
+                  <ListIcon as={FaUserCheck} color="green.500" />
+                  dsad*** - 95,000원 입찰 하셨습니다.
+                </div>
+                <span className="text-sm">1시간 전</span>
+              </ListItem>
+            </List>
+          </div>
         </section>
-        <section className="bg-amber-800 flex flex-col gap-4 w-1/2">
-          <div className="flex gap-4">
-            <h1>경매 아이템 이름</h1>
+        <section className="flex flex-col gap-6 w-1/2 px-6">
+          <div className="flex gap-4 items-center pt-4">
+            <h1 className="text-3xl">경매 아이템 이름</h1>
             <span>남은 기간</span>
           </div>
           <div className="flex gap-4">
@@ -58,10 +133,10 @@ const AuctionDetail = () => {
               neque laborum. Praesentium eveniet culpa inventore.
             </span>
           </div>
-          <div className="flex gap-4">
+          <div className="flex gap-6">
             <div className="flex flex-col gap-2">
               <span>거래 방법</span>
-              <div className="flex gap-2 text-16px">
+              <div className="flex gap-2 text-sm">
                 <button className="outline outline-black text-white bg-black py-1 px-4">
                   택배 거래 / 착불
                 </button>
@@ -78,73 +153,105 @@ const AuctionDetail = () => {
             </div>
           </div>
           <div className="flex flex-col items-end gap-3">
-            <div>
-              사용 가능 포인트
-              <span className="text-3xl">200,000P</span>
+            <div className="flex gap-4 items-end">
+              <span className="text-sm">사용 가능 포인트</span>
+              <span className="text-xl">200,000P</span>
             </div>
-            <form className="w-full flex border border-black">
+            <form className="w-full h-10 flex border border-black">
               <input
                 type="text"
                 placeholder="입찰하실 포인트를 적어주세요."
-                className="w-4/5 h-8 border-r border-black"
+                className="w-4/5 border-r border-black p-3"
               />
-              <button className="w-1/5 h-8 bg-white outline-black">입찰하기</button>
+              <button className="w-1/5 bg-white outline-black">입찰하기</button>
             </form>
-            <button className="w-full h-8 bg-black text-white">즉시 구매</button>
+            <button className="w-full h-10 bg-black text-white">즉시 구매</button>
           </div>
           <div className="flex flex-col">
-            <div className="flex justify-between items-center py-4">
+            <div className="flex justify-between items-end py-4">
               <span className="text-4xl">Q&A</span>
               <span>질문 작성하기</span>
             </div>
-            <Accordion allowToggle>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box as="span" flex="1" textAlign="left">
-                      Section 1 title - 아이디1**
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </AccordionPanel>
-              </AccordionItem>
+            <div className="h-64 overflow-y-scroll no-scrollbar">
+              <Accordion allowToggle>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as="span" flex="1" textAlign="left">
+                        Section 1 title - 아이디1**
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </AccordionPanel>
+                </AccordionItem>
 
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box as="span" flex="1" textAlign="left">
-                      Section 2 title - 아이디2**
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </AccordionPanel>
-              </AccordionItem>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box as="span" flex="1" textAlign="left">
-                      Section 2 title - 아이디3**
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as="span" flex="1" textAlign="left">
+                        Section 2 title - 아이디2**
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as="span" flex="1" textAlign="left">
+                        Section 2 title - 아이디3**
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as="span" flex="1" textAlign="left">
+                        Section 2 title - 아이디3**
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as="span" flex="1" textAlign="left">
+                        Section 2 title - 아이디3**
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </div>
           </div>
         </section>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,26 @@ export default {
         '18px': '18px',
         '16px': '16px',
       },
+      aspectRatio: {
+        '4/5': '4/5',
+      },
+      width: {
+        '480px': '480px',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          /* firefox */
+          'scrollbar-width': 'none',
+          /* safari and chrome */
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+        },
+      });
+    },
+  ],
 };
