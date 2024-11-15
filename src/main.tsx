91f1af3c4b4cf948b98cd1d06db54ee6b3759c50
Oh-Donghan <odonghan94@gmail.com>
2024-11-15 12:34:11 +0900

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider, customTheme } from './config/ChakraProvider';
 import './index.css';
-// import { worker } from './mocks/browser';
+import { worker } from './mocks/browser';
 import RecoilProvider from './recoil/store';
 
-// if (import.meta.env.MODE !== 'development') {
-//   worker.start({
-//     onUnhandledRequest: 'bypass', // 처리되지 않은 요청을 무시하고 네트워크로 전달
-//   });
-// }
+if (import.meta.env.MODE === 'development') {
+  worker.start({
+    onUnhandledRequest: 'bypass', // 처리되지 않은 요청을 무시하고 네트워크로 전달
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RecoilProvider>
