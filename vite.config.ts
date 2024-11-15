b380877dad706c78eaee5f020a7c474232d1630a
Oh-Donghan <odonghan94@gmail.com>
2024-11-15 11:42:46 +0900

    설정에 소스맵 활성화

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  server: {
-    https: true,
-  },
   plugins: [react(), mkcert()],
+  build: {
+    sourcemap: true,
+  },
 });
